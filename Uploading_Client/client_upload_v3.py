112a57e51e011a809839af6340c14c5578f8b067
AlbertoBarraja <barraja.alberto@gmail.com>
2014-08-15 21:44:33 +0300

    add new version of the uploading client version beta

--- Uploading_Client/client_upload_v3.py
+++ Uploading_Client/client_upload_v3.py
@@ -86,8 +86,10 @@ def getBandWith(segName, httpServer):
 		logging.error(message)
 	
 	t2 = time.time()
+	#print "time_1: "+str(t1)+" time_2: "+str(t2)
 	#print response.read()
 	timeInterval = float(t2-t1)
+	#print timeInterval
 	currentBandwith = float(segLenght*8/(t2-t1))
 	return (currentBandwith,timeInterval)
 
@@ -156,13 +158,19 @@ missing = 0
 
 #------------------------------------------ WORKING POINT -------------------------------------------------#
 
-def getNextSize(layer, segment):	
+def getNextSize(layer):	
+	current_segment= segment_indexes[layer]
 	next_layer = layer+1
+	#print " layer is"+ str(next_layer)										#DEBUG
 	next_segment = segment_indexes[next_layer]
-	#inital_increase = 0
-	while ( (next_segment != segment) or (next_layer == layersNum ) ):
+	#print " segment "+ str(next_layer)+" next_segment "+ str(next_segment)	#DEBUG
+
+	while(next_segment==current_segment):
 		next_layer = next_layer +1
+		#print " layer is"+ str(next_layer)										#DEBUG
 		next_segment = segment_indexes[next_layer]
+		#print " segment "+ str(next_layer)+" next_segment "+ str(next_segment)	#DEBUG
+		
 	segName = segTable[next_layer][next_segment]
 	length = os.path.getsize(segName)
 	return (length,segName,next_layer)
@@ -172,41 +180,53 @@ start_layer = 0
 upload_time = 0
 count = segment_indexes[start_layer]
 
-print "---------------------------------------------------------------"
-
+print "------------------------ START UPLOAD -------------------------"
+time_start= time.time();
 while(count<numSeg):
+	print "------------------------ SEG "+str(count)+" LAY "+str(start_layer)+" -------------------------"
+
 	next_name = segTable[start_layer][count]
 	current_layer = start_layer
-	session_time = 0 
+	session_time = 0
+	tmp_forecast= 0 
+	forecast_time=0
 
-	while(session_time<2):
+	while((session_time<2) or (forecast_time <2)):
 		currBW, upload_time  = getBandWith(next_name, httpServer)
-		message = str(datetime.datetime.now().time()) + "	segment [" + str(count) + "] | layer [" + str(start_layer) + "] | threshold: [ " + str((layerBW[start_layer]/8)/1024) + "KB/s] | bandwitdth: [" + str((currBW/8)/1024) + " KB/s] | upload time: ["+str(upload_time)+" s]" 
-		logging.info(message)
+		#message = str(datetime.datetime.now().time()) + "	segment [" + str(count) + "] | layer [" + str(start_layer) + "] | threshold: [ " + str((layerBW[start_layer]/8)/1024) + "KB/s] | bandwitdth: [" + str((currBW/8)/1024) + " KB/s] | upload time: ["+str(upload_time)+" s]" 
+		#logging.info(message)
+		#print "session_time: "+str(session_time)+" forecast_time: "+str(forecast_time)+" upload_time: "+str(upload_time)+" segment_forecast_time: "+str(tmp_forecast)
 		segment_indexes[current_layer] = segment_indexes[current_layer] + 1 
 		#print segment_indexes
 		session_time = float(session_time + upload_time)
 		if(current_layer< (layersNum - 1) ):
-			next_length,next_name,current_layer = getNextSize(current_layer, count)  
-			session_time= float (session_time+(next_length/currBW))
+			next_length,next_name,current_layer = getNextSize(current_layer)
+			tmp_forecast= float(next_length/currBW)
+			forecast_time= float (session_time+(next_length/currBW))
 		else:
-			print session_time
-			print "---------------------------------------------------------------"
 			break
 	
 	count = count + 1
+	print "session_time: "+str(session_time)+" forecast_time: "+str(forecast_time)
+	print "---------------------------------------------------------------"
 
 	if(session_time<2):
 		leftover= float (2-session_time)
+		print"wait "+ str(leftover)
+		print "---------------------------------------------------------------"
 		sleep(leftover) # time in seconds
 
 
 	if (count == numSeg ):
-		print "------------------------ LAYER 0 OVER -------------------------"
+		print "------------------------ LAYER OVER -------------------------"
 		start_layer = start_layer +1
 		if (start_layer < layersNum):
 				count = segment_indexes[start_layer]
 	
 
 print " "
+print segment_indexes
+time_end=time.time()
+delta =float(time_end-time_start)
+print delta
 print "--- END --- "
